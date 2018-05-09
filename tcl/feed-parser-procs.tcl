67a4a83aaca8bcdbd53033c8d92ad17308d1123b
Hector Romojaro <hector.romojaro@gmail.com>
2018-05-09 15:33:31 +0000

    Revert massive replacement of empty list creation sentences. The use of '[list]' instead of '{}' adds semantics that could be used for performance improvements in the future, such as using a different internal representation. There is already work in this direction, avoiding the generation of the string representation during comparison of empty strings (huge thanks to Stefan Sobernig for the pointer: https://core.tcl.tk/tcl/info/44527c632ed609c2).

--- tcl/feed-parser-procs.tcl
+++ tcl/feed-parser-procs.tcl
@@ -29,7 +29,7 @@ ad_proc -public feed_parser::sort_result {
 } {
     @author Simon Carstensen
 } {
-    set sorted {}
+    set sorted [list]
     for {set i 0} {$i < [llength $result]} {incr i} {
 	lappend sorted [lindex $result end-$i]
     }
@@ -330,7 +330,7 @@ ad_proc -public feed_parser::parse_feed {
 
         set item_nodes [feed_parser::items_fetch -doc_node $doc_node]
         set item_nodes [feed_parser::sort_result -result $item_nodes]
-        set items {}
+        set items [list]
 
         foreach item_node $item_nodes {
             lappend items [feed_parser::item_parse -item_node $item_node]
